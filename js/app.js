b24f6343947b2731ec4d83183e901b68878a8bc0
PhynxMorales <80421800+PhynxMorales@users.noreply.github.com>
2022-08-09 17:38:54 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 window.onscroll = function () {
   if (document.documentElement.scrollTop > 100) {
+    document.querySelector(".navbar-eipsa").classList.add("show-nav");
     document.querySelector(".header-eipsa").classList.add("show-header");
     document.getElementById("logo-eipsa").src =
       "https://cdn.shopify.com/s/files/1/0040/8551/4369/files/Logo_EIPSA_color.svg?v=1660004820";
   } else {
     document.querySelector(".header-eipsa").classList.remove("show-header");
+    document.querySelector(".navbar-eipsa").classList.remove("show-navbar");
     document.getElementById("logo-eipsa").src =
       "https://cdn.shopify.com/s/files/1/0040/8551/4369/files/Logo_EIPSA_blanco.svg?v=1660004532";
   }
